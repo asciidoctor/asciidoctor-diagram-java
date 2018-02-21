f6e206b6ce2c087776362af7211c7e522aa0b66c
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2018-02-21 09:24:45 +0100

    Remove code duplicated Ditaamini code from Ditaa generator

--- ditaa/src/main/java/org/asciidoctor/diagram/ditaa/Ditaa.java
+++ ditaa/src/main/java/org/asciidoctor/diagram/ditaa/Ditaa.java
@@ -1,6 +1,7 @@
 package org.asciidoctor.diagram.ditaa;
 
 import org.asciidoctor.diagram.*;
+import org.stathissideris.ascii2image.core.CommandLineConverter;
 import org.stathissideris.ascii2image.core.ConversionOptions;
 import org.stathissideris.ascii2image.graphics.BitmapRenderer;
 import org.stathissideris.ascii2image.text.TextGrid;
@@ -12,10 +13,14 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Ditaa implements DiagramGenerator
 {
     public static final MimeType DEFAULT_OUTPUT_FORMAT = MimeType.PNG;
+    public static final MimeType DEFAULT_CONTENT_TYPE = MimeType.TEXT_PLAIN_UTF8;
+    public static final String DEFAULT_CHARSET = "UTF-8";
 
     @Override
     public String getName()
@@ -26,38 +31,50 @@ public class Ditaa implements DiagramGenerator
     @Override
     public ResponseData generate(Request request) throws IOException {
         MimeType format = request.headers.getValue(HTTPHeader.ACCEPT);
-
         if (format == null) {
             format = DEFAULT_OUTPUT_FORMAT;
         }
 
-        if (!format.equals(MimeType.PNG)) {
+        if (!format.equals(MimeType.PNG) && !format.equals(MimeType.SVG)) {
             throw new IOException("Unsupported output format: " + format);
+        }
+
+        MimeType contentType = request.headers.getValue(HTTPHeader.CONTENT_TYPE);
+        if (contentType == null) {
+            contentType = DEFAULT_CONTENT_TYPE;
+        }
+
+        String charset = contentType.parameters.get("charset");
+        if (charset == null) {
+            charset = DEFAULT_CHARSET;
+        }
+
+        java.util.List<String> options = new ArrayList();
+        options.add("--encoding");
+        options.add(charset);
+
+        if (format.equals(MimeType.SVG)) {
+            options.add("--svg");
         }
 
         ConversionOptions conversionOptions;
 
         String optionString = request.headers.getValue(HTTPHeader.OPTIONS);
         if (optionString != null) {
-            String[] options = optionString.split(" ");
-            conversionOptions = ConversionOptions.parseCommandLineOptions(options);
-        } else {
-            conversionOptions = new ConversionOptions();
+            options.addAll(Arrays.asList(optionString.split(" ")));
         }
-        
-        TextGrid textGrid = new TextGrid();
-        textGrid.initialiseWithText(request.asString(), conversionOptions.processingOptions);
 
-        org.stathissideris.ascii2image.graphics.Diagram diagram = new org.stathissideris.ascii2image.graphics.Diagram(textGrid, conversionOptions);
-        BufferedImage image = new BitmapRenderer().renderToImage(diagram, conversionOptions.renderingOptions);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        CommandLineConverter.convert(
+                options.toArray(new String[0]),
+                new ByteArrayInputStream(request.data),
+                output
+        );
+        output.close();
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        MemoryCacheImageOutputStream cacheStream = new MemoryCacheImageOutputStream(out);
-        ImageIO.write(image, "png", out);
-        cacheStream.flush();
         return new ResponseData(
                 format,
-                out.toByteArray()
+                output.toByteArray()
         );
     }
 }
