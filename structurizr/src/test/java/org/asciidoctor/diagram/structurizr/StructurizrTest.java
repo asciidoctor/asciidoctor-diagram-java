83f2ebff08bdb98987a4f6f482d3649f3b6f37ad
Lars Uffmann <lars.uffmann@gmail.com>
2024-01-12 12:13:41 +0100

    Introduce dedicates test for includes.

--- structurizr/src/test/java/org/asciidoctor/diagram/structurizr/StructurizrTest.java
+++ structurizr/src/test/java/org/asciidoctor/diagram/structurizr/StructurizrTest.java
@@ -12,6 +12,37 @@ import java.net.URI;
 class StructurizrTest {
     private static final String INPUT = """
             workspace {
+                model {
+                    user = person "User" "A user of my software system."
+                    softwareSystem = softwareSystem "Software System" "My software system."
+            
+                    user -> softwareSystem "Uses"
+                }
+            
+                views {
+                    systemContext softwareSystem "SystemContext" {
+                        include *
+                        autoLayout
+                    }
+            
+                    styles {
+                        element "Software System" {
+                            background #1168bd
+                            color #ffffff
+                        }
+                        element "Person" {
+                            shape person
+                            background #08427b
+                            color #ffffff
+                        }
+                    }
+                }
+               \s
+            }\
+            """;
+
+    private static final String INPUT_WITH_INCLUDE = """
+            workspace {
                 !include dsl-include.dsl
                 model {
                     user = person "User" "A user of my software system."
@@ -49,7 +80,6 @@ class StructurizrTest {
         h.putValue(HTTPHeader.CONTENT_TYPE, MimeType.TEXT_PLAIN_UTF8);
         h.putValue(HTTPHeader.ACCEPT, Structurizr.PLANTUML);
         h.putValue(Structurizr.VIEW_HEADER, "SystemContext");
-        h.putValue("X-Structurizr-IncludeDir", Path.of("src/test/resources/stdin.dsl").toAbsolutePath().toString());
 
         ResponseData responseData = new Structurizr().generate(new Request(
                 URI.create("/structurizr"),
@@ -61,6 +91,29 @@ class StructurizrTest {
 
         String text = new String(responseData.data, responseData.format.parameters.getOrDefault("charset", "utf-8"));
         assertTrue(text.startsWith("@startuml"));
+    }
+
+    @Test
+    void includesShouldBeResolved() throws IOException
+    {
+        HTTPHeaders h = new HTTPHeaders();
+        h.putValue(HTTPHeader.CONTENT_TYPE, MimeType.TEXT_PLAIN_UTF8);
+        h.putValue(HTTPHeader.ACCEPT, Structurizr.PLANTUML);
+        h.putValue(Structurizr.VIEW_HEADER, "SystemContext");
+        // path needs to be a (non-existing) file inside the targeted directory.
+        h.putValue("X-Structurizr-IncludeDir", Path.of("src/test/resources/stdin").toAbsolutePath().toString());
+
+        ResponseData responseData = new Structurizr().generate(new Request(
+            URI.create("/structurizr"),
+            h,
+            INPUT_WITH_INCLUDE.getBytes(Charsets.UTF8)
+        ));
+
+        assertTrue(responseData.format.isSameType(Structurizr.PLANTUML));
+
+        String text = new String(responseData.data, responseData.format.parameters.getOrDefault("charset", "utf-8"));
+        assertTrue(text.startsWith("@startuml"));
         assertTrue(text.contains("SOAP/XML"));
     }
+
 }
